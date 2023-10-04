3d58b30a9d2ef293afdd0e9629e59a7668e67006
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2023-10-03 18:52:45 -0600

    cambios en el buscador

--- src/pages/directorio.tsx
+++ src/pages/directorio.tsx
@@ -1,6 +1,6 @@
 import { TextInput } from "@mantine/core";
 import { useDebouncedValue } from "@mantine/hooks";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { DirectorioCard, TextDivider } from "~/components";
 import {
   directorioDireccion,
@@ -13,6 +13,54 @@ const Directorio = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [searchDebounced] = useDebouncedValue(searchTerm, 400);
 
+  const direccion_filtrado = useMemo(() => {
+    const result = directorioDireccion
+      .filter((d) => d.nombre.toLowerCase().includes(searchDebounced))
+      .map((directorio) => (
+        <DirectorioCard key={directorio.id} {...directorio} />
+      ));
+    if (result.length > 0) {
+      return result;
+    }
+    return <h3 className="font-bold">No se encontraron resultados</h3>;
+  }, [searchDebounced]);
+
+  const subdireccion_filtrado = useMemo(() => {
+    const result = directorioSubDireccion
+      .filter((d) => d.nombre.toLowerCase().includes(searchDebounced))
+      .map((directorio) => (
+        <DirectorioCard key={directorio.id} {...directorio} />
+      ));
+    if (result.length > 0) {
+      return result;
+    }
+    return <h3 className="font-bold">No se encontraron resultados</h3>;
+  }, [searchDebounced]);
+
+  const subdireccion_administrativa_filtrado = useMemo(() => {
+    const result = directorioSubdireccionAdministrativa
+      .filter((d) => d.nombre.toLowerCase().includes(searchDebounced))
+      .map((directorio) => (
+        <DirectorioCard key={directorio.id} {...directorio} />
+      ));
+    if (result.length > 0) {
+      return result;
+    }
+    return <h3 className="font-bold">No se encontraron resultados</h3>;
+  }, [searchDebounced]);
+
+  const subdireccion_servicios_educativos_filtrado = useMemo(() => {
+    const result = directorioSubdireccionServiciosEducativos
+      .filter((d) => d.nombre.toLowerCase().includes(searchDebounced))
+      .map((directorio) => (
+        <DirectorioCard key={directorio.id} {...directorio} />
+      ));
+    if (result.length > 0) {
+      return result;
+    }
+    return <h3 className="font-bold">No se encontraron resultados</h3>;
+  }, [searchDebounced]);
+
   return (
     <div className="container m-auto px-5">
       <h3 className="mt-7 text-center text-2xl font-bold">Directorio</h3>
@@ -24,32 +72,16 @@ const Directorio = () => {
       />
 
       <TextDivider text="DIRECCIÓN" />
-      {directorioDireccion
-        .filter((d) => d.nombre.toLowerCase().includes(searchDebounced))
-        .map((directorio) => (
-          <DirectorioCard key={directorio.id} {...directorio} />
-        ))}
+      {direccion_filtrado}
 
       <TextDivider text="SUBDIRECCIÓN ACADÉMICA" />
-      {directorioSubDireccion
-        .filter((d) => d.nombre.toLowerCase().includes(searchDebounced))
-        .map((directorio) => (
-          <DirectorioCard key={directorio.id} {...directorio} />
-        ))}
+      {subdireccion_filtrado}
 
       <TextDivider text="SUBDIRECCIÓN ADMINISTRATIVA" />
-      {directorioSubdireccionAdministrativa
-        .filter((d) => d.nombre.toLowerCase().includes(searchDebounced))
-        .map((directorio) => (
-          <DirectorioCard key={directorio.id} {...directorio} />
-        ))}
+      {subdireccion_administrativa_filtrado}
 
       <TextDivider text="SUBDIRECCIÓN DE SERVICIOS EDUCATIVOS E INTEGRACIÓN SOCIAL" />
-      {directorioSubdireccionServiciosEducativos
-        .filter((d) => d.nombre.toLowerCase().includes(searchDebounced))
-        .map((directorio) => (
-          <DirectorioCard key={directorio.id} {...directorio} />
-        ))}
+      {subdireccion_servicios_educativos_filtrado}
     </div>
   );
 };
